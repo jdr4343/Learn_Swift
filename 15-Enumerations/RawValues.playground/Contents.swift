d865e8164bec0e8f1f546fdd748f81fbdbfcb305
jdr4343 <jdr4343@naver.com>
2021-07-12 19:16:09 +0900

    RawValues

--- 15-Enumerations/RawValues.playground/Contents.swift
+++ 15-Enumerations/RawValues.playground/Contents.swift
@@ -25,16 +25,49 @@ import UIKit
  # Raw Values
  ![Raw Values](raw-values.png)
  */
+// 여기에서 선언한 case는 모두 독립적인 값입니다. enum 케이스는 그자체로 독립적인 값이지만 내부에 또다른 값을 저장할수 있습니다 이것을. RowValue라고 하는데 우리말로는 원시값이라고 번역한니다.
+//RowValue는 필수사항이 아닙니다. RowValue을 저장하지 않는다고 해서 에러가 발생하는 것은 아닙니다.
+//enum 케이스와 연관된 또다른 값을 함께 저장해야할떄 선택적으로 사용합니다.
+
+enum Alignment: String {
+    case left = "left"
+    case right = "right"
+    case center = "center"
+}
+
+//원시값에 접근해 보겠습니다.
+Alignment.left.rawValue
+Alignment.right.rawValue
 
 
+enum Number: Int {
+    case first
+    case second = 101
+    case third
+}
+
+Number.third.rawValue
+//보는 것처럼 102 입니다. 원시값을 지정하지 않으면 자동으로 값이 지정되는데 기준값은 항상 이전값입니다. 그래서 101에서 1이 증가한 102가 저장 되었습니다.
+
+//RowValue는 선언이후에 값을 바꾸는 건 불가능 합니다.
+
+//생성자
+Number(rawValue: 0)
+Number(rawValue: 200)//값이 없는경우 nil을 리턴합
+Alignment(rawValue: "left")
 
 
+enum Weekday: String {
+    case sunday
+    case monday
+    case thesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+}
 
-
-
-
-
-
+Weekday.sunday.rawValue
 
 
 
