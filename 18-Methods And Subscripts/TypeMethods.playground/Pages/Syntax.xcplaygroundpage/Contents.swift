0da14c9c29097d5e7ceb6c55f5d7a0f6950d1cf7
jdr4343 <jdr4343@naver.com>
2021-07-14 01:45:29 +0900

    Type Methods

--- 18-Methods And Subscripts/TypeMethods.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
+++ 18-Methods And Subscripts/TypeMethods.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
@@ -27,11 +27,39 @@ import UIKit
  ![class-func](class-func.png)
  ![call](call.png)
  */
+//형식과 연관된 메소드를 구현하고 호출하는 방법
+//속성을 인스턴스 속성과 타입 속성으로 구분하는것처럼
+//메소드도 인스턴스 메소드와 타입메소드로 구분합니다.타입 메소드는 인스턴스가 아닌 형식에 연관된 메소드 입니다.
+//class 키워드를 사용할때는 subclass에서 오버라이딩을 허용할때 사용합니다.
+//static으로 선언 하면 오버라이딩이 금지됩니다
 
+class Circle {
+    //타입 속성
+    static let pi = 3.14
+    //인스턴스 속성
+    var radius = 0.0
+    //인스턴스 메소드 추가
+    func getArea() -> Double {
+        return radius * radius * Circle.pi
+        //이렇게 인스턴스 속성은 이름만으로 접근할수 있지만 타입속성을 타입이름으로 접근해야합니다.
+    }
+    //타입 메소드 추가
+    class func printPi() {
+        print(pi)
+        //타입 메소드에서는 타입 이름없이 타입속성에 바로 접근할수 있습니다. 반면 인스턴스 멤버에 직접 접근하는 방법은 없습니다.
+    }
+}
 
+//타입 메소드는 타입이름으로 호출
+Circle.printPi()
 
-
-
+class strokeCircle: Circle {
+    override static func printPi() {
+        print(pi)
+        //static 키워드로 선언한 메소드를 서브 클래스에서 override 할수없습니다.
+        //subclass 에서 오버라이딩하는 메소드는 static키워드가 아니라 class 키워드로 선언합니다
+    }
+}
 
 
 
