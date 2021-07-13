073b66d91dda5ddb90c5a153cb0a02303becce4e
jdr4343 <jdr4343@naver.com>
2021-07-13 17:40:01 +0900

    ProtocolType

--- 22-Protocols/ProtocolTypes.playground/Pages/Collections of Protocol Types.xcplaygroundpage/Contents.swift
+++ 22-Protocols/ProtocolTypes.playground/Pages/Collections of Protocol Types.xcplaygroundpage/Contents.swift
@@ -26,7 +26,7 @@ import Foundation
 /*:
  # Collections of Protocol Types
  */
-
+//프로토콜을 활용해서 상속과 유사한 패턴을 구현 해보겠습니다.
 
 protocol Figure {
    func draw()
@@ -55,8 +55,21 @@ struct Circle: Figure {
 let t = Triangle()
 let r = Rectangle()
 let c = Circle()
-
-
+//모든 인스턴스를 하나의 배열에 저장해 보겠습니다.
+//let list = [t, r, c]
+//인스턴스의 형식이 모두 다르고 심지어 값 형식과 참조 형식이 혼재 되어 있기 때문에 에러가 발생합니다.
+//만약 인스턴스의 형식이 동일한 상속계층에 있는 클래스로 구현되어 있다면 자동으로 업 캐스팅되어 저장 되었겠지만 이코드는 그렇지 못합니다.
+//하지만 세형식은 Figure 프로토콜을 공통적으로 채용하고 있습니다. 프로토콜은 First class citizen 이기 때문에 형식으로 사용할수 있습니다.
+let Figurelist: [Figure] = [t, r, c]
+//이렇게 배열형식을 Figure 배열로 저장하면 모든 인스턴스를 저장할수 있습니다.
+//인스턴스가 값인지 객체인지 상관하지 않고 Figure 형식으로 캐스팅 해서 저장합니다.
+for item in Figurelist {
+    item .draw()
+    //공통적으로 들어간 draw는 사용할수 있지만 Circle의 radius는 사용할수 없습니다. 이런경우 as? 연산자를 활용해서 원래 형식으로 캐스팅 해야됩니다. 
+    if let c  = item as? Circle {
+        c.radius
+    }
+}
 
 
 
--- 22-Protocols/ProtocolTypes.playground/Pages/Protocol Types.xcplaygroundpage/Contents.swift
+++ 22-Protocols/ProtocolTypes.playground/Pages/Protocol Types.xcplaygroundpage/Contents.swift
@@ -25,6 +25,9 @@ import Foundation
 /*:
  # Protocol Types
  */
+//프로토콜 타입,타입캐스팅 활용 적합성 확인
+//프로토콜은 First class citizen 입니다. 다시 말해서 독립적인 형식입니다.
+//변수나 상수를 선언할때 자료형으로 선언 하거나 파라미터의 자료형으로 사용할수있고 리턴형으로 사용하는것도 가능합니다.
 
 protocol Resettable {
    func reset()
@@ -39,9 +42,22 @@ class Size: Resettable {
       height = 0.0
    }
 }
+//새로운 인스턴스를 생성하겠습니다.
+let s = Size()
+//이번에는 상수의 형식을 Resettable로 선언하겠습니다.
+let r: Resettable = Size()
+//프로토콜은 형식으로 사용될수 있고 Resettable 프로토콜을 채용하고 있는 Size 인스턴스를 Resettable 형식으로 저장할수 있습니다.
+//이것은 superclass 형식으로 저장하는 upcasting과 유사합니다.
 
-
-
+//upcasting 한 경우에는 해당 클래스에 선언되어있는 메모만 사용할수 있습니다.
+//여기에서도 마찬가지입니다. 프로토콜 형식으로 저장하면 프로토콜애 선언되어 있는 멤버만 사용할수 있습니다.
+//c상수에는 실제로 Size인스턴스가 저장되어 있지만 width 속성이나 height 속성에 접근해보면 에러가 발생합니다.
+//반면 프로토콜에 선언되어 있는 reset 메소드를 호출하는것은 가능합니다.
+//값형식은 상속이 불가능하지만 프로토콜을 사용하면 이렇게 상속과 유사한 패턴을 구현 할수있습니다.
+let b = Size()
+let c: Resettable = Size()
+//c.width
+c.reset()
 
 
 
