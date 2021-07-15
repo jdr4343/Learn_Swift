1f922795cf61c78ffe8fffa698924089b877fa24
jdr4343 <jdr4343@naver.com>
2021-07-15 20:52:52 +0900

    Property Wrapper basic

--- 17-Properties/PropertyWrapper.playground/Pages/Page1.xcplaygroundpage/Contents.swift
+++ 17-Properties/PropertyWrapper.playground/Pages/Page1.xcplaygroundpage/Contents.swift
@@ -25,6 +25,11 @@ import Foundation
 /*:
  # Property Wrapper
  */
+//속성에 접근하는 코드를 별도의 타입으로 분리하는 PropertyWrapper에 대해 알아봅시다.
+//보통 속성을 선언할때는 StoredProperties 형태로 선언합니다. 하지만 속성에 값을 저장하는 방식을 직접 구현하거나 속성이 리턴하는 값을 제어 하려면 ComputedProperties 형태로 선언합니다.ComputedProperties에는 값을 리턴하는 get블록과 값을 저장하는 set블록이 포함되는데 두블록에서 구현하는 코드는 보통 비슷한 패턴의 코드가 반복됩니다.
+//Property Wrapper는 속성에 접근하는 코드를 별도의 타입으로 분리하고 여러곳에서 재사용할수 있도록 도와주는 특성입니다.
+
+//구조체에 포함된 두속성은 ComputedProperties 선언되어있습니다. 그리고 설정데이터를 영구적으로 저장하기 위해서 UserDefaults를 사용하고 있습니다. get 블록에서는 UserDefaults 저장되어 있는데이터를 리턴하고 있고 set 블록에선 속성에 저장된 값을 UserDefaults에 저장하고 있습니다.
 
 struct PlayerSetting {
    var initialSpeed: Double {
@@ -35,7 +40,7 @@ struct PlayerSetting {
          UserDefaults.standard.set(newValue, forKey: "initialSpeed")
       }
    }
-   
+    //속성의 수가 늘어나면 비슷한 패턴의 코드를 반복적으로 구현해야 합니다. 그리고 문자열키를 사용하고 있기때문에 오타로인한 문자가 발생할 가능성도 있습니다.오늘 공부할 Property Wrapper를 사용하면 속성에 접근하는 코드를  별도의 타입으로 분리할수 있습니다. 이렇게 분리한 코드는 특성을 활용해서 반복적으로 활용할수 있기때문에 코드가 훨씬 단순해 집니다 그리고 잘못된 키로 인한 문제도 거의 발생하지 않습니다.
    var supportGesture: Bool {
       get {
          return UserDefaults.standard.bool(forKey: "supportGesture")
--- 17-Properties/PropertyWrapper.playground/Pages/Page2.xcplaygroundpage/Contents.swift
+++ 17-Properties/PropertyWrapper.playground/Pages/Page2.xcplaygroundpage/Contents.swift
@@ -28,15 +28,38 @@ import Foundation
 /*:
 # Property Wrapper
 */
-
+//Property Wrapper를 구현해 보겠습니다.
+//먼저 이전 페이지에서 ComputedProperties 구현했던 코드를 별도의 타입으로 분리 하겠습니다.
 
 struct PlayerSetting {
+    //여기에 Property Wrapper를 적용하겠습니다 우리가 Property Wrapper를 구현하면 타입이름과 동일한 특성이 제공 됩니다.
+    //이제 부터 initialSpeed에 저장하는 값은 메모이에 저장되는 것이 아니라 UserDefault의 set에서 구현한 방식으로 UserDefault에 저장됩니다. 마찬가지로 initialSpeed 속성에 접근하면 get에서 구현한 방식으로 값을 리턴합니다.이렇게 동작하는데 필요한 코드는 컴파일러가 컴파일 시점에 자동으로 추가합니다.
+    @UserDefaultsHelper(key: "initialSpeed", defaultValue: 1.0)
    var initialSpeed: Double
+    @UserDefaultsHelper(key: "supportGesture", defaultValue: true)
    var supportGesture: Bool
+}
+//특성을 하나 추가 하겠습니다. 이특성은 타입을 PropertyWrapper로 만들어 주는 특성입니다 이특성은 구조체 열거형 클래스 선언에서 같은 방법으로 사용할수 있습니다.
+@propertyWrapper
+struct UserDefaultsHelper<Value> {//모든 타입을 지원할수 있도록 제네릭 타입으로 선언하겠습니다.
+    let key: String
+    let defaultValue: Value
+    //위의 두 코드는 일반적인 타입이라면 아무런 문제가 없는 코드인데 Property Wrapper 특성을 추가 했기 때문에 에러메시지가 설명하는 것처럼 wrappedValue라는 특별한 특성을 하나 추가해야합니다.
+    var wrappedValue: Value {
+        //유저 디폴트에서 값을 읽어오고 값을 저장하는 코드를 구현하겠습니다.
+        get {
+            UserDefaults.standard.object(forKey: key) as? Value ?? defaultValue//읽어온 값을 Value타입으로 타입 캐스팅해서 읽어오고 리턴 할수 없다면 default value를 리턴하겠습니다.
+        }
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: key)
+        }
+    }
 }
 
 
-var currentSetting = PlayerSetting(initialSpeed: 1.0, supportGesture: true)
+
+
+var currentSetting = PlayerSetting()
 
 currentSetting.initialSpeed
 currentSetting.initialSpeed = 1.5
@@ -46,7 +69,8 @@ currentSetting.supportGesture
 currentSetting.supportGesture = false
 currentSetting.supportGesture
 
-
+//이전 페이지에 있는 코드와 비교하면 훨씬 단순해 졌습니다.
+//새로운 속성을 추가하고 같은 방법으로 관리하고 싶다면 Property Wrapper 만든 특성을 추가하고 키와 디폴트 밸류를 전달하는 것으로 끝납니다 그리고 나중에 값을 관리하는 방식을 바꾸고 싶다면 Property Wrapper 형식의 구조체만 수정하면 나머지 부분은 자동으로 바뀝니다.
 
 
 
