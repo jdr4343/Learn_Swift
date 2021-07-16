706b504ba5c5f1962b1cb7b92686027498e238ed
jdr4343 <jdr4343@naver.com>
2021-07-17 06:22:41 +0900

    Initializer Inheritance

--- 20-Initialization/ClassInitializers.playground/Pages/Class Initializers.xcplaygroundpage/Contents.swift
+++ 20-Initialization/ClassInitializers.playground/Pages/Class Initializers.xcplaygroundpage/Contents.swift
@@ -28,8 +28,47 @@ import UIKit
  # Convenience Initializer
  ![convenience](convenience.png)
  */
+//클래스에서 구현하는 특별한 생성자에 대해 공부합시다.
+
+//Designated Initializer 지정 생성자 는 메인 Initializer 입니다. 여기서 말하는 메인은 클래스가 가진 모든 속성을 초기화 한다는 뜻입니다.
+//class 에서 Initializer를 생성할 경우 한가지 역활이 추가됩니다. Initializer 실행이 완료되기 전에 슈퍼 클래스에 Designated Initializer 를 추가 해야합니다.이것을 Initializer Delegation이라고 합니다.
+//class 에서 구현할수있는 Designated Initializer 수에는 제한이 없으나 대부분의 경우하나만 구현 합니다 default Initializer를 사용하거나 superClass 로부터 Designated Initializer 를 상속했다면 Designated Initializer 직접 구현할 필요는 없습니다. 하지만 나머지 경우에는 하나이상의 Designated Initializer 가 필요합니다.
+
+class Position {
+    var x: Double
+    var y: Double
+    //지금 에러가나는 이유는 두가지 입니다.모든 속성이 기본값을 가지고 있지않고 생성자를 구현하지 않았기 때문입니다 여기에 모든 속성을 초기화 하는 Designated Initializer 를 구현 하겠습니다.
+    init(x: Double, y:Double) {
+        //파라미터 이름과 속성이름이 동일하기 때문에 self를 사용해야 합니다.
+        self.x = x
+        self.y = y
+        //핵심은 모든 속성을 초기화 해야합니다.
+    }
+}
 
 
+//Convenience Initializer 간편 생성자
+//init 키워드 앞에 Convenience가 추가되는걸 빼면 문법적인 차이는 없습니다.
+//Convenience Initializer는 다양한 초기화 방법을 구현하기 위한 유틸리티 성격의 Initializer 입니다.
+//Designated Initializer 와는 달리 모든 속성을 초기화 해야하는것은 아닙니다. 보통 필요한 속성만 추가한다음에 class 에 있는 다른 Initializer를 호출해서 나머지 초기화를 완료 하도록 구현합니다.
+//class가 상속관계에 포함되어 있는 경우를 생각해보겠습니다. Designted Initializer는 슈퍼클래스에 Designted Initializer 를 호출해야 합니다 반면 Convenience Initializer는 슈퍼클래스에 있는 Initializer 를 호출할수 없습니다.
+//반드시 동일한 클래스에 있는 Initializer를 호출해서 최종적으로 Designted Initializer가 호출되도록 구현해야 합니다.
+
+//x만 초기화하는 Convenience Initializer 구현해보겠습니다.
+class num {
+    var x: Int
+    var y: Int
+    
+    init(x:Int, y:Int) {
+        self.x = x
+        self.y = y
+    }
+    convenience init(x: Int) {
+        self.init(x: x, y: 2)
+        //만약 여기에서 직접 x 속성을 초기화 하면 Designated Initializer에 있는 코드와 중복됩니다.그래서 Designated Initializer로 호출한다음에 x에는 파라미터를 그대로 전달하고 y에는 기본값을 전달하고 있습니다.이렇게 하면 초기화 코드가 중복되지 않습니다.
+        //Convenience Initializer 는 보통 이런 패턴으로 구현합니다. Initializer를 호출하는 문법은 동일합니다. 
+    }
+}
 
 
 
--- 20-Initialization/ClassInitializers.playground/Pages/Initializer Inheritance.xcplaygroundpage/Contents.swift
+++ 20-Initialization/ClassInitializers.playground/Pages/Initializer Inheritance.xcplaygroundpage/Contents.swift
@@ -26,7 +26,10 @@ import Foundation
 /*:
  # Initializer Inheritance
  */
-class Figure {
+//Initializer는 다른 멤버와 마찬가지로 서브클래스로 상속됩니다.
+//다른멤버는 조건없이 상속되지만 Initializer는 상속되는 조건이 까다롭습니다. 기본적으로 슈퍼클래스에서 구현한 Initializer는 서브클래스로 상속되지 않습니다.
+
+class Figure {//name 속성을 초기화 하는 Initializer가 구현되어 있습니다.
    var name: String
 
    init(name: String) {
@@ -36,13 +39,47 @@ class Figure {
    func draw() {
       print("draw \(name)")
    }
+    //Figure class에 Convenience initializer 키워드를 추가하겠습니다.
+    convenience init() {
+        self.init(name: "unknown")
+    }
 }
 
-class Rectangle: Figure {
-   var width: Double
-   var height: Double
+class Rectangle: Figure {//두개의 속성이 선언되어 있습니다.
+    var width: Double = 0.0
+    var height: Double = 0.0
+    
+    init(name: String, width: Double, height: Double) {//앞에서 Designated Initializer는 모든 속성을 초기화 해야한다고 배웠습니다. 그래서 이렇게 모두 초기화 했는데 에러가 발생합니다.
+        //만약 클래스가 다른 클래스를 상속하지 않았다면 올바른 코드지만 지금은 상속계층에 포함되어 있기때문에 다른방식으로 구현합니다.
+        //일단 이코드에는 두가지 문제가 있습니다. 우선 슈퍼클래스에 Designated Initializer 를 호출하지 않고 있습니다. 그리고 상위 구현을 호출하기 전에 상속받은 name 속성에 접근하고 있습니다.
+        //이런건 문법적으로 허용되지 않습니다. 보통은 현재 클래스에 선언된 속성만 초기화 하고 나머지는 슈퍼 클래스에 맡깁니다.
+        super.init(name: name)//이렇게 현재 클래스에 있는 속성을 초기화 한다음에 상위구현을 호출합니다.
+        self.width = width
+        self.height = height
+    }
+    //이번에는 상속받은 Initializer 오버라이딩 해보겠습니다.오버라이딩 강의 에서 배웠던 것처럼 init 키워드 앞에 오버라이딩 키워드를 추가했습니다. 방금 오버라이딩한 initializer는 Designated initializer 입니다.
+    //그래서 조금전과 마찬가지로 현재 클래스에 있는 속성을 초기화 한다음에 상위구현을 호출하고 있습니다.
+    override init(name: String) {
+        width = 0
+        height = 0
+        super.init(name: name)
+    }
+    //이번에는 Designed initialazer와는 달리 오버라이드 키워드를 추가하지 않았습니다. 그런데 에러는 발생하지 않죠 convenience initializer는 항상 동일한 클래스에 있는 다른 initializer를 호출합니다. 그리고 슈퍼클래스에 있는 initializer 을 직접호출하는 것은 불가는 합니다. 그래서 오버라이딩이라는 개념이 적용되지 않습니다.
+    convenience init() {
+        self.init(name: "unknown")
+    }
 }
+//이속성들은 기본값을 가지지 않고 있기때문에 초기화가 필요합니다.
+//이상태에서 Initializer가 상속되었다고 가정해보곘습니다.
+//Figure에선 name 속성만 초기화 하고 있죠?만약 Rectangle을 상속된 Initializer 로 초기화 한다면 width,height 속성은 여전히 초기화 되지 않은 상태로 남아있습니다.
+//스위프트에서는 이런건 초기화 실패 입니다.지금처럼 상속된 Initializer가 일부속성만 초기화하는것을 막기 위해서 Initializer가 상속되지 않는 것입니다.
 
+//스위프트 두가지 규칙에 따라 Initializer를 상속합니다.
+//첫번째 서브클래스에 모든 속성이 기본값으로 초기화 되어 있고 Designated Initializer 직접 구현하지 않았다면 슈퍼클래스에 있는 모든 Designted Initializer가 상속됩니다.
+//Ractengle 클래스에 선언되어 있는 속성에 기본값을 지정하겠습니다.이제 상속된 Initializer 를 사용해도 일부속성만 초기화 되는 문제는 발생하지 않습니다.
+
+//두번째 서브클래스가 모든 Designated Initializer를 상속 받았거나 오버라이딩 했다면 모든 Conveniece initaializer가 상속됩니다.
+//Ractengle 클래스에 Designated Initializer 를 추가 하겠습니다.
 
 
 
